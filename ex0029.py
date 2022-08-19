08def92eb471b1e5339bba4623285afb94092579
Natã Silva de Assis <srassis2014@gmail.com>
2022-08-18 22:32:45 -0300

    codigo melhorado

--- ex0029.py
+++ ex0029.py
@@ -3,8 +3,8 @@ velocidade = float(input('\033[1;32mInforme a velocidade do carro:\033[m'))
 if velocidade > 80:
     percentualExcedido = (velocidade - 80) * 100 / 80
     multa = percentualExcedido * 7.00
-    print(f'A velocidade foi \033[1;32;30m{velocidade}\033[m km/h '
-          f'\n A multa de\033[1;32;30m 7.00\033[m por km/h a cima'
-          f'\n O total a pagar vai ser \033[1;32;30m{multa}\033[m reais')
+    print(f'A sua velocidade foi de \033[1;32;30m{velocidade}\033[m km/h '
+          f'\n Com a multa de\033[1;32;30m 7.00\033[m por km/h a cima'
+          f'\n Total de: \033[1;32;30m{multa}\033[m reais')
 else:
-    print('\033[1;35mFique Tranquilo, você não precisa pagar multa !\033[m')
+    print('\033[1;35m Fique Tranquilo,Não consta multa no sistema !\033[m')
